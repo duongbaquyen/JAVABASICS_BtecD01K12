e9ca23a9e1e5f7fe4d9cc57bb7e0cfe7abca4f51
duongbaquyen <93500214+duongbaquyen@users.noreply.github.com>
2021-11-02 22:10:20 +0700

    Initial commit

---
+++ src/main/java/B02_datatypes_control_loop/Main_helloworld.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package B02_datatypes_control_loop;
+
+/**
+ *
+ * @author Duong Thi Bich Hong
+ */
+public class Main_helloworld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
